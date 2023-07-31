b7a9cf266eac333447b6645fff599fa634daa99e
Shakhawat33 <mdshakhawat99o@gmail.com>
2023-07-31 16:48:33 +0600

    first commit

---
+++ assienment.js
@@ -0,0 +1,71 @@
+// ১. Variable কি ?
+ Ans: Variables are Containers for Storing Data
+
+// ২. Variable কিভাবে লিখতে হয় ?
+Ans:  var friendsName = "shakhawat 0185456854 ";
+      console.log(friendsName)
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+Ans: জাভাস্ক্রিপ্ট স্ট্রিংগুলি টেক্সট সংরক্ষণ এবং ম্যানিপুলেট করার জন্য 
+
+exp:     var strType = "shakhawat pro";
+         console.log(typeof strType) 
+         etc:'',"",`` 
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+Ans:জাভাস্ক্রিপ্টের সংখ্যা মাত্র এক প্রকার। সংখ্যাগুলি দশমিক সহ বা ছাড়া লেখা যেতে পারে।
+
+exp:  var num = 20;
+      console.log(numtype)
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+Ans:bollean type is true and false .
+    
+exp:  var BoolType = true ;
+      console.log(typeof BoolType)
+
+      var BoolType = false ;
+      console.log(typeof BoolType)
+
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+Ans: var rochona ="FSUFUFGBIJGYFG UHUIHAUG UYFUYHVIU UUEHFUHUVHUHF UHUOWHFUGUGVU UY UIAFHYUOFH UYOHFUH HUFHU UHFOUUEFUG GIUFUUGLUH DSSEUUFU UDHFUOHUH EFHIUHUIH IUEWGTFLUHFEUUG IUU HUHUOH UDEHFUGUAGU UEHUIHV UHUDH UI UU FU UUA UDHFUH ";
+     console.log(rochona.toLocaleLowerCase())
+
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+Ans: Operator	 Description
+
+       +	     Addition
+       -	     Subtraction
+       *	     Multiplication
+       **	     Exponentiation
+       %	     Modulus (Division Remainder)
+       ++	     Increment
+       --	     Decrement
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+Ans:  var num1 = -40 ;
+      var sum = Math.abs(num1);
+      console.log(sum);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+Ans: var num1 = 0.0001 ;
+     var sum = Math.ceil(num1);
+     console.log(sum);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+Ans: var num1 = 0.0005;
+     var sum = Math.floor(num1);
+     console.log(sum);  
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+Ans:;var num1 = 0.51254;
+var sum = Math.round(num1);
+console.log(sum);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন 
+     var num1 =  ;
+     var sum = Math.random(num1)*100;
+     console.log(sum);
---
+++ hello.js
@@ -0,0 +1,54 @@
+//  var friendsName = "shakhawat 0185456854 ";
+//  console.log(friendsName)
+
+
+
+//  var strType = "shakhawat pro";
+//  console.log( typeof strType) 
+
+//  var numType = 01962889333 ;
+//  console.log(typeof numType)
+
+//  var BoolType = true ;
+//  console.log(typeof BoolType)
+
+//  var rochona ="FSUFUFGBIJGYFG UHUIHAUG UYFUYHVIU UUEHFUHUVHUHF UHUOWHFUGUGVU UY UIAFHYUOFH UYOHFUH HUFHU UHFOUUEFUG GIUFUUGLUH DSSEUUFU UDHFUOHUH EFHIUHUIH IUEWGTFLUHFEUUG IUU HUHUOH UDEHFUGUAGU UEHUIHV UHUDH UI UU FU UUA UDHFUH ";
+// console.log(rochona.toLocaleLowerCase())
+
+
+//  var myFrd ="Shakhawat,Adnan,Antor,Rafi ,Atik ";
+
+//  console.log(myFrd.split("Atik"));
+
+
+//  var myFrd ="Shakhawat Adnan Antor Rafi Atik ";
+//  console.log(myFrd.indexOf("Atik"));
+
+
+// var num1 ="54";
+// var num2 = 89 ;
+
+// num1 = parseFloat(num1);
+
+// var total =num1+num2;
+// console.log(total)
+ 
+
+// var num3 = 20 ;
+// var num4 = 40 ;
+
+// // num3 = ""+num3;
+
+// // console.log(typeof num3)
+
+// var total = num3+num4;
+// console.log(total);
+
+
+// var num1 = -40 ;
+// var sum = Math.abs(num1);
+// console.log(sum);
+
+
+
+
